bd095d6fb1aacfca088184416a9258c2b93cccb3
aakashDeveloper <ahanda205@gmail.com>
2019-01-29 21:59:23 +0530

    a

---
+++ javascript/five.js
@@ -0,0 +1,55 @@
+var city = ["london", "newyork", "davos", "mumbai", "delhi"]
+
+for(i=5; i<10; i++){
+    console.log(i)
+}
+
+var city = ["london", "newyork", "davos", "mumbai", "delhi"]
+for(i=0;i<city.lenght;i++){
+    console.log(city[i])
+}
+
+var a = [1,2,3,4,5]
+for(i=0;i<a.lenght;i++){
+    console.log(city[i])
+}
+
+var a = [1,2,3,4,5,6,7,8,9,10,11,12]
+for(i=0; i<a.length; i++){
+    if(a[i]%4 == 0){
+        console.log("even")
+    }else {
+        console.log(a[i])
+    }
+}
+
+
+function add(a,b){
+    return a+b
+}
+
+add(1,2)
+
+function isEven(b){
+    var out;
+    if(b%2 == 0){
+        out = "event"
+    }else {
+        out = "odd"
+    }
+    return out
+}
+
+function add(a){
+    var z = 10
+    var out = z+a
+    return out
+}
+
+clouser
+
+
+var y  = 111
+function check(a){
+    return y
+}
