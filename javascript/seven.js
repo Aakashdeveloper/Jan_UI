6fff1ab0360bf882b5e69f8e896a75a3dd6521d3
aakashDeveloper <ahanda205@gmail.com>
2019-01-31 22:04:08 +0530

    a

---
+++ javascript/seven.js
@@ -0,0 +1,130 @@
+var nn= ["john", "andy", "meo"];
+
+var a = [2,4,6,7]
+
+a.map(Math.sqrt)
+
+var nn= ["john", "andy", "meo"];
+nn.map((a) => { return a.toUpperCase()});
+
+
+
+Math.PI
+3.141592653589793
+var a = Math.PI
+undefined
+a
+3.141592653589793
+Math.random()
+0.9766770738665036
+Math.random()
+0.6318234903664304
+Math.random()
+0.044994529821094975
+Math.random()
+0.5818241009339382
+Math.random()*100
+62.571599440971035
+Math.floor(Math.random()*100)
+13
+Math.floor(Math.random()*100)
+60
+Math.floor(Math.random()*100)
+34
+Math.floor(1.45)
+1
+Math.floor(1.75)
+1
+Math.floor(1.95)
+1
+Math.ceil(1)
+1
+Math.ceil(1.3)
+2
+Math.ceil(1.9)
+2
+Math.ceil(1.1)
+2
+Math.round(2.6)
+3
+Math.round(2.5)
+3
+Math.round(2.1)
+2
+Math.round(2.4)
+2
+Math.round(2.5)
+3
+
+Math.trunc(1.4)
+1
+Math.trunc(1.7)
+1
+Math.trunc(1.9)
+1
+Math.trunc(2)
+2
+Math.trunc(2.7)
+2
+Math.trunc(3)
+3
+Math.pow(2,2)
+4
+Math.pow(2,5)
+32
+Math.min(2,5,7,1,6)
+1
+Math.max(2,5,7,1,6)
+7
+Math.exp(2,2)
+7.38905609893065
+Math.exp(2,2,2)
+7.38905609893065
+
+Math.cos()
+NaN
+Math.cos(1)
+0.5403023058681398
+Math.tan(1)
+1.5574077246549023
+Math.sin(1)
+0.8414709848078965
+Math.log(3)
+1.0986122886681096
+
+
+input from the user and you will generate one random number
+and you do floor of that number if both number match you will
+say win else say loose 
+
+
+var a = "london"
+
+switch(a){
+    case "delhi":
+        console.log("wrong part")
+        break;
+    case "london":
+        console.log("right path")
+        break;
+    default:
+        console.log("i dont know")
+}
+
+
+var a = "10"
+undefined
+var b = 20
+undefined
+a+b
+"1020"
+parseInt(a)+b
+30
+var c = 1.354
+undefined
+var c = "1.354"
+undefined
+parseInt(c)+b
+21
+parseFloat(c)+b
+21.354
