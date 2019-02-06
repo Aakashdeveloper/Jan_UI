8c8e10898246018c9e287a80d0ef81a986ce82c7
aakashDeveloper <ahanda205@gmail.com>
2019-02-06 21:44:16 +0530

    a

---
+++ javascript/onmouseover.js
@@ -0,0 +1,19 @@
+function mouseO(bb){
+    bb.style.backgroundColor="red";
+    bb.innerHTML="Release Me";
+}
+
+function mouseOut(a){
+    a.style.backgroundColor="green";
+    a.innerHTML="Over Me";
+}
+
+function mouseD(bb){
+    bb.style.backgroundColor="teal";
+    bb.innerHTML="Release Me";
+}
+
+function mouseU(a){
+    a.style.backgroundColor="tomato";
+    a.innerHTML="Click Me";
+}
